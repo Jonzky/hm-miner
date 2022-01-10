8dacc81c860ee6e698a08097debda0a0afa035b4
Jon Zakay <JonZakay@aol.com>
2022-01-10 10:01:00 +0000

    Testing which config is used

--- start-miner.sh
+++ start-miner.sh
@@ -6,24 +6,21 @@ if [ "$(df -h /var/data/ | tail -1 | awk '{print $5}' | tr -d '%')" -ge 80 ]; th
     rm -rf /var/data/*
 fi
 
-# It looks like this is not used
-# # If a url has been set use it, otherwise use the default
-# if [[ -z "${OVERRIDE_CONFIG_URL}" ]]; then
-#   cp miner.config /opt/miner/releases/$HELIUM_GA_RELEASE/sys.config
-# else
-#   wget -O "/opt/miner/releases/$HELIUM_GA_RELEASE/sys.config" \
-#       "${OVERRIDE_CONFIG_URL}"
-# fi
-
 # If a url has been set use it, otherwise use the default
 if [[ -z "${OVERRIDE_CONFIG_URL}" ]]; then
-  echo "No override provided for miner config"
+  cp miner.config /opt/miner/releases/$HELIUM_GA_RELEASE/sys.config
 else
-  wget -O "/opt/miner/config/sys.config" \
+  wget -O "/opt/miner/releases/$HELIUM_GA_RELEASE/sys.config" \
       "${OVERRIDE_CONFIG_URL}"
 fi
 
-
+# If a url has been set use it, otherwise use the default
+if [[ -z "${OVERRIDE_CONFIG_URL_TEST}" ]]; then
+  echo "No override provided for miner config"
+else
+  wget -O "/opt/miner/config/sys.config" \
+      "${OVERRIDE_CONFIG_URL_TEST}"
+fi
 
 # Wait for the diagnostics app to be loaded
 until wget -q -T 10 -O - http://diagnostics:5000/initFile.txt > /dev/null 2>&1
